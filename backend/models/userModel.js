8cc8a3ef50c14d9020019acfbd3820422759493a
ScarlettRed6 <jcaamrelo@gmail.com>
2025-11-03 08:57:59 +0800

    Backend: initial setup app and server

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+import pool from "../config/db.js";
+
+export async function findUserByEmail(email){
+    const result = await pool.query(
+        `SELECT * FROM users WHERE email = $1`, [email]
+    );
+    return result.rows[0];
+}
+
+export async function createUser(name, email, hashedPassword){
+    const result = await pool.query(
+        `INSERT INTO users (name, email, password)
+        VALUES ($1, $2, $3) RETURNING *`,
+        [name, email, hashedPassword]
+    );
+    return result.rows[0];
+}
+
+
+
+
