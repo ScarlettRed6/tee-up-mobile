0f8829dea896cbfea22fca7b7d6811689ddff835
ScarlettRed6 <jcaamrelo@gmail.com>
2025-11-07 14:10:29 +0800

    Feat: findUserById

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,18 @@
 import pool from "../config/db.js";
 
+//FINDER FUNCTIONS
 export async function findUserByEmail(email){
     const result = await pool.query(
         `SELECT * FROM users WHERE email = $1`, [email]
     );
     return result.rows[0];
+}
+
+export async function findUserById(id){
+    const user = await pool.query(
+        `SELECT * FROM users WHERE id = $1`, [id]
+    );
+    return user.rows[0];
 }
 
 export async function createUser(name, email, hashedPassword){
@@ -15,7 +23,5 @@ export async function createUser(name, email, hashedPassword){
     );
     return result.rows[0];
 }
-
-
 
 
