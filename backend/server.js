9a7057dee07d785028cf37993e7d6f35b1b46f54
ScarlettRed6 <jcaamrelo@gmail.com>
2025-11-09 22:01:54 +0800

    Util: event logger

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import logger from "./utils/eventLogger.js"
 
 dotenv.config();
 
@@ -7,5 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    logger.emit("log", `Server started successfully! Running on Port ${PORT}`);
 });
 
