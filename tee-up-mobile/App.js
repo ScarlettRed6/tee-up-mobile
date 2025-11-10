f785d7183ed7ca5fda66b5e7c837967d20aaeb3a
ScarlettRed6 <jcaamrelo@gmail.com>
2025-11-10 22:44:38 +0800

    Fixed: untouchable keyboard

--- tee-up-mobile/App.js
+++ tee-up-mobile/App.js
@@ -21,10 +21,8 @@ export default function App() {
 
   return (
     <AuthProvider>
-      <View style={{ flex: 1 }}>
-        <StatusBar style="dark" />
-          <AppNavigator />
-      </View>
+      <StatusBar style="dark" />
+      <AppNavigator />
     </AuthProvider>
   );
 }
