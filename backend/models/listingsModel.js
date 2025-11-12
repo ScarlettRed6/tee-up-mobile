ccb33c65aed9094a77af69fa6f4a8f0316a28b15
ScarlettRed6 <jcaamrelo@gmail.com>
2025-11-12 15:40:39 +0800

    Refactored: listings model

--- backend/models/listingsModel.js
+++ backend/models/listingsModel.js
@@ -3,7 +3,7 @@ import pool from "../config/db.js";
 //Create a new listing
 export async function insertListing(user_id, title, description, category, brand, condition, price, status, photos){
    const newListing = await pool.query(
-        `INSERT INTO listing (user_id, title, description, category, brand, condition, price, status, photos)
+        `INSERT INTO listings (user_id, title, description, category, brand, condition, price, status, photos)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
         [user_id, title, description, category, brand, condition, price, status, photos]
    );
@@ -12,13 +12,13 @@ export async function insertListing(user_id, title, description, category, brand
 
 //GETS
 export async function getAllListings(){
-    const allListings = await pool.query(`SELECT * FROM listing`);
+    const allListings = await pool.query(`SELECT * FROM listings`);
     return allListings.rows;
 }
 
 export async function getListingById(id) {
     const result = await pool.query(
-        `SELECT * FROM listing WHERE listing_id = $1`, [id]
+        `SELECT * FROM listings WHERE listing_id = $1`, [id]
     );
     return result.rows[0];
 }
@@ -26,7 +26,7 @@ export async function getListingById(id) {
 //UPDATES
 export async function updateListing(id, title, description, category, brand, condition, price, status, photos) {
     const result = await pool.query(
-        `UPDATE listing SET title = $1, description = $2, category = $3, brand = $4, condition = $5, price = $6, status = $7, photos = $8
+        `UPDATE listings SET title = $1, description = $2, category = $3, brand = $4, condition = $5, price = $6, status = $7, photos = $8
         WHERE listing_id = $9 RETURNING *`,
         [title, description, category, brand, condition, price, status, photos, id]
     );
@@ -35,6 +35,6 @@ export async function updateListing(id, title, description, category, brand, con
 
 //DELETES
 export async function deleteListing(id) {
-    const result = await pool.query(`DELETE FROM listing WHERE listing_id = $1 RETURNING *`, [id]);
+    const result = await pool.query(`DELETE FROM listings WHERE listing_id = $1 RETURNING *`, [id]);
     return result.rows[0];
 }
