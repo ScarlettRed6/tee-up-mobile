45c22a4955ed2ccda4399c1018155cabe774d92c
ScarlettRed6 <jcaamrelo@gmail.com>
2025-11-13 19:28:32 +0800

    Feat: listingsapi

--- tee-up-mobile/api/listingsApi.js
+++ tee-up-mobile/api/listingsApi.js
@@ -2,11 +2,16 @@ import api from "./axiosInstance.js"
 
 export const createListing = async (listingData) => {
     const res = await api.post("/listings", listingData);
-    return res.data;
+    return res.data.listing;
 };
 
-export const getAllListings= async () => {
-    const res = await api.get("/listings");
-    return res.data.result;
-};
+export async function fetchListings(filters = {}) {
+    const params = new URLSearchParams(filters).toString();
+    const response = await api.get(`/listings?${params}`);
+    return response.data.result;
+}
 
+export async function fetchUserListings(userId){
+    const response = await api.get(`/listings?user_d=${userId}`);
+    return response.data.result;
+}
