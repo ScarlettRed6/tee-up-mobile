e97b4c0c324c1244d50ddb0b6e8bda6d3bb4cdca
ScarlettRed6 <jcaamrelo@gmail.com>
2025-11-20 21:41:40 +0800

    Feat: getUserById public route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,3 +11,21 @@ export async function getUserProfile(req, res){
         res.status(500).json({ error: err.message });
     }
 }//End of getUserProfile function
+
+export async function getUserById(req, res){
+    try{
+        const userId = req.params.id;
+        const user = await findUserById(userId);
+
+        if(!user){
+            console.log("USERCONTROLLER: USER DOES NOT EXIST");
+            return res.status(404).json({ message: "User not found" });
+        }
+        
+        return res.json(user);
+    }catch(err){
+        console.error("(USERCONTROLLER) ERROR FETCHING USER BY ID: ", err);
+        return res.status(500).json({ message: "Fetching error" });
+    }
+}//End of getUserById function
+
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { getUserProfile } from "../controllers/userController.js";
+import { getUserProfile, getUserById } from "../controllers/userController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 //User routes
 router.get("/profile", verifyToken, getUserProfile);
+
+//For the public route
+router.get("/:id", getUserById);
 
 
 export default router;
