8afe8500b7b8bda0a3b4d0c1d13a39d216f42e2d
ScarlettRed6 <jcaamrelo@gmail.com>
2026-02-15 16:17:39 +0800

    [ADMIN CONTROLLER] added create admin function

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -9,3 +9,40 @@ import {
     storeRefreshToken
 } from "../models/userModel";
 
+export async function createAdmin(req, res) {
+    const { name, email, password, role } = req.body;
+    const superAdminId = req.user.id;
+    try {
+        //Validate user role
+        if(role !== 'admin' || role !== 'superadmin') {
+            console.log("[ADMIN CONTROLLER] Invalid Role. Must be 'admin' or 'superadmin'");
+            return res.status(400).json({ message: "Invalid Role. Must be 'admin' or 'superadmin'" });
+        }
+        //Check if admin user email already exists in the database
+        const existingUser = await findUserByEmail(email);
+        if (existingUser) {
+            console.log("[ADMIN CONTROLLER] Email already exists.");
+            return res.status(400).json({ message: "Email already exists."});
+        }
+
+        //Begin hashing the password with bcrypt
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const adminUser = await createAdminUser(name, email, hashedPassword, role);
+
+        console.log("[ADMIN CONTROLLER] Successfully created an admin user.");
+        res.status(201).json({
+            message: `${role === 'superadmin' ? 'Superadmin' : 'Admin'} created successfully`,
+            user: {
+                id: adminUser.id,
+                name: adminUser.name,
+                email: adminUser.email,
+                role: adminUser.role
+            }
+        });
+    } catch (error) {
+        console.error("[ADMIN CONTROLLER] Error creating an admin:", error);
+        res.status(500).json({ error: error.message });
+    }
+}//End of createAdmin function
+
