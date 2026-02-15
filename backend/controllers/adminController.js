e2f015bbf4d63f37a5a80778d32863086ac263df
ScarlettRed6 <jcaamrelo@gmail.com>
2026-02-15 16:38:48 +0800

    [ADMIN CONTROLLER] added getter for all admin users and also an update function for admin role

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -46,3 +46,62 @@ export async function createAdmin(req, res) {
     }
 }//End of createAdmin function
 
+export async function getAdmins(req, res) {
+    try {
+        const admins = await getAllAdmins();
+
+        console.log("[ADMIN CONTROLLER] Successfully fetched all admin users");
+        res.json({ admins });
+    } catch (error) {
+        console.error("[ADMIN CONTROLLER] Error getting all admin users.");
+        res.status(500).json({ error: error.message });
+    }
+}//End of getAdmins function
+
+//Updates admin role (only a user with superadmin role can use this control)
+export async function updateAdminRole(req, res) {
+    const { userId } = req.params;
+    const { role } = req.body;
+    //NOTE: CHECK LATER WHY ROLE NEEDS TO BE VALIDATED IF ADMIN OR SUPERADMIN FOR 
+    //UPDATING THE ROLE
+    try {
+        if (role !== 'admin' && role !== 'superadmin') {
+            console.log("[ADMIN CONTROLLER] Invalid role. Must be 'admin' or 'superadmin'");
+            return res.status(400).json({ message: "Invalid role. Must be 'admin' or 'superadmin'" });
+        }
+
+        const user = await findUserById(userId);
+        if(!user){
+            console.log("[ADMIN CONTROLLER] User not found.");
+            return res.status(404).json({ message: "User not found." });
+        }
+        //Check if the user who requested the update is an admin
+        if(user.role !== 'admin' && user.role !== 'superadmin') {
+            console.log("[ADMIN CONTROLLER] User trying to update is not an admin");
+            return res.status(400).json({ message: "User trying to update is not an admin" });
+        }
+        //Prevent superadmin from demoting themselves
+        if(parseInt(userId) === req.user.id && role !== 'superadmin'){
+            console.log("[ADMIN CONTROLLER] superadmin user cannot demote themselves.");
+            return res.status(400).json({ message: "superadmin user cannot demote themselves" });
+        }
+        
+        const updatedUser = await updateUserRole(userId, role);
+
+        console.log("[ADMIN CONTROLLER] Successfully updated admin role.");
+        res.json({
+            message: "Admin role updated successfully",
+            user: {
+                id: updatedUser.id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                role: updatedUser.role
+            }
+        });
+    } catch (error) {
+        console.error("[ADMIN CONTROLLER] Error updating admin role.");
+        res.status(500).json({ error: error.message });
+    }
+}//End of updateAdminRole function
+
+
