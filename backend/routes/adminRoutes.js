36eb8a9b6eda3c1aeb29ce0fa7d94eeb032c39b5
ScarlettRed6 <jcaamrelo@gmail.com>
2026-02-19 10:02:37 +0800

    [ADMIN ROUTES] Feat/routes: added the protected routes with the middleware

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -7,4 +7,12 @@ import {
 } from "../controllers/adminController.js";
 import { verifyAdmin, verifySuperAdmin } from "../middleware/authMiddleware.js";
 
+const router = express.Router();
 
+router.get("/profile", verifyAdmin, getAdminProfile);
+
+router.post("/create", verifySuperAdmin, createAdmin);
+router.get("/all", verifySuperAdmin, getAdmins);
+router.put("/:userId/role", verifySuperAdmin, updateAdminRole);
+
+export default router;
