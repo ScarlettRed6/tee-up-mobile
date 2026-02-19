8f25bd7bb9632cea7b8e9ebf739eba492aabb22b
ScarlettRed6 <jcaamrelo@gmail.com>
2026-02-19 23:35:13 +0800

    [USER ROUTES] Feat/routes: added routes for admin control

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,16 @@
 import express from "express";
-import { getUserProfile, getUserById, updateUserProfile } from "../controllers/userController.js";
-import { verifyToken } from "../middleware/authMiddleware.js";
+import { 
+    getUserProfile, 
+    getUserById, 
+    updateUserProfile,
+    suspendUser,
+    unsuspendUser,
+    deleteUser,
+    adminUpdateUser,
+    getUsers,
+    getSuspensionLogs
+} from "../controllers/userController.js";
+import { verifyToken, verifyAdmin, verifySuperAdmin } from "../middleware/authMiddleware.js";
 import upload from "../middleware/upload.js";
 
 const router = express.Router();
@@ -12,5 +22,15 @@ router.put("/update", verifyToken, upload.single("profile_image"), updateUserPro
 //For the public route - must be last to avoid catching other routes
 router.get("/:id", getUserById);
 
+//ADMIN ROUTES - user manangement
+router.get("/admin/users", verifyAdmin, getUsers);
+router.get("/admin/suspension-logs", verifyAdmin, getSuspensionLogs);
+router.get("/admin/:id", verifyAdmin, getUserById);
+router.get("/admin/profile", verifyToken, verifyAdmin, getUserProfile);
+
+router.put("/admin/update/:id", verifyAdmin, upload.single("profile_image"), adminUpdateUser);
+router.post("/admin/suspend/:id", verifyAdmin, suspendUser);
+router.post("/admin/unsuspend/:id", verifyAdmin, unsuspendUser);
+router.delete("/admin/delete/:id", verifyAdmin, deleteUser);
 
 export default router;
