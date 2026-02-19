73274e1ac64d5e002295190e614acefa25e0ced5
ScarlettRed6 <jcaamrelo@gmail.com>
2026-02-19 22:20:17 +0800

    [REPORT ROUTES] Feat/routes: added the routes for admin report control functions

--- backend/routes/reportsRoutes.js
+++ backend/routes/reportsRoutes.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { verifyToken } from "../middleware/authMiddleware.js";
-import { reportListing, reportUser } from "../controllers/reportsController.js";
+import { verifyToken, verifyAdmin } from "../middleware/authMiddleware.js";
+import { 
+    reportListing, 
+    reportUser,
+    getAdminReports,
+    reviewReport 
+} from "../controllers/reportsController.js";
 import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.post("/listing/:listing_id", verifyToken, upload.single("photo"), reportListing);
 router.post("/user/:user_id", verifyToken, upload.single("photo"), reportUser);
+
+//Admin report routes
+router.get("/admin/all", verifyToken, verifyAdmin, getAdminReports);
+router.put("/admin/review/:report_id", verifyToken, verifyAdmin, reviewReport);
 
 export default router;
